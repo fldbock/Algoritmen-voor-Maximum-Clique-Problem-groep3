9769d3a47028a604078f8a6cc2161cbbb4e18774
simonreyntjens <simon.reyntjens@ugent.be>
2022-02-22 19:59:59 +0100

    Update EffectiveLocalSearch.java

--- src/EffectiveLocalSearch.java
+++ src/EffectiveLocalSearch.java
@@ -1,5 +1,15 @@
+import graphlib.graphs.UndirectedGraph;
+import graphlib.edges.UndirectedEdge;
+import graphlib.nodes.Node;
+import graphlib.edges.Edge;
+
+import java.io.*;
+import java.net.URL;
+import java.util.*;
+
 public class EffectiveLocalSearch {
-    public void effectiveLocalSearch() {
+
+    public void effectiveLocalSearch(UndirectedGraph cc) {
 
     }
 }
